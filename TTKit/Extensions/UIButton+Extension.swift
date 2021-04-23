877bde7a41ea1d0316c4c0df896c5a719b016b6f
ttdp <ttdp@foxmail.com>
2021-04-23 08:23:42 +0800

    add rotation button

---
+++ TTKit/Extensions/UIButton+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  UIButton+Extension.swift
+//  TTKit
+//
+//  Created by Tian Tong on 2021/4/23.
+//  Copyright © 2021 TTDP. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func addAnimation() {
+        addTarget(self, action: #selector(animateDown), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(animateUp), for: [.touchDragExit, .touchCancel, .touchUpInside, .touchUpOutside])
+    }
+    
+    @objc private func animateDown(sender: UIButton) {
+        alpha = 0.85
+        animate(sender, transform: CGAffineTransform.identity.scaledBy(x: 0.95, y: 0.95))
+    }
+    
+    @objc private func animateUp(sender: UIButton) {
+        alpha = 1.0
+        animate(sender, transform: .identity)
+    }
+    
+    private func animate(_ button: UIButton, transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 3,
+                       options: [.curveEaseInOut],
+                       animations: {
+                        button.transform = transform
+                       }, completion: nil)
+    }
+    
+}
--- TTKit/Views/FloatingActionViewController.swift
+++ TTKit/Views/FloatingActionViewController.swift
@@ -8,10 +8,58 @@
 
 import UIKit
 
+class FloatButton: UIButton {
+    
+    var alphaBefore: CGFloat = 1
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        alphaBefore = alpha
+        
+        UIView.animate(withDuration: 0.3) {
+            self.alpha = 0.4
+            
+            if self.transform == .identity {
+                self.transform = CGAffineTransform(rotationAngle: 45 * (.pi / 180))
+            } else {
+                self.transform = .identity
+            }
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.35) {
+            self.alpha = self.alphaBefore
+        }
+    }
+    
+}
+
 class FloatingActionViewController: BaseViewController {
     
+    // MARK: - View
+    
+    lazy var floatButton: UIButton = {
+        let button = FloatButton()
+        button.backgroundColor = .lightGray
+        button.setTitle("Action", for: .normal)
+//        button.addAnimation()
+        return button
+    }()
+    
     override func setupViews() {
-        view.backgroundColor = .red
+        view.addSubview(floatButton)
+        view.addConstraints(format: "H:[v0(100)]", views: floatButton)
+        view.addConstraints(format: "V:[v0(100)]", views: floatButton)
+        floatButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        floatButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    // MARK: - Action
+    
+    @objc func toggleButton() {
+        
     }
     
 }
