3761e53ba6d8f259e202dcd7fe8d5dea9e733ab3
ttdp <ttdp@foxmail.com>
2021-07-03 22:26:22 +0800

    prepare text code

--- TTKit/Views/HTTPSSLViewController.swift
+++ TTKit/Views/HTTPSSLViewController.swift
@@ -27,16 +27,39 @@ class HTTPSSLViewController: BaseViewController {
         
         dataTask.resume()
         
-        if let data = AF.request("https://ag.bizersoft.com/api/users/1000000038").response {
-            print("AF", data)
-        }
-        
         AF.request("https://ag.bizersoft.com/api/users/1000000038").response { response in
             if let data = response.data {
-                let json = try! JSONSerialization.jsonObject(with: data, options: [])
-                print(json)
+                let _ = try! JSONSerialization.jsonObject(with: data, options: [])
             }
         }
+    }
+    
+    let imageView = UIImageView()
+    
+    override func setupViews() {
+        imageView.backgroundColor = .lightGray
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        view.addConstraints(format: "H:[v0(200)]", views: imageView)
+        view.addConstraints(format: "V:[v0(200)]", views: imageView)
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        let http = HTTP(base: "https://ag.bizersoft.com")
+        http.get("/static/avatar/100/000/00/1000000038.jpg") { response in
+            DispatchQueue.main.async {
+                if let data = response.data {
+                    self.imageView.image = UIImage(data: data)
+                }
+            }
+        }
+        
+        let task = http.fileRequest(downloadPath: "/static/avatar/100/000/00/1000000038.jpg") { url, error in
+            
+        }
+        
+        task.go()
     }
     
 }
